6099bb9a5b7e4236ef82abea1abec64de6e5fcf0
Heiko Seeberger <git@heikoseeberger.de>
2024-01-01 19:30:40 +0100

    feat: re-introduce graceful shutdown

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -3,8 +3,12 @@ mod v0;
 use anyhow::{Context, Result};
 use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
 use serde::Deserialize;
-use std::net::IpAddr;
-use tokio::net::TcpListener;
+use std::{net::IpAddr, time::Duration};
+use tokio::{
+    net::TcpListener,
+    signal::unix::{signal, SignalKind},
+    time::sleep,
+};
 use tower::ServiceBuilder;
 use tower_http::trace::TraceLayer;
 
@@ -13,10 +17,16 @@ use tower_http::trace::TraceLayer;
 pub struct Config {
     addr: IpAddr,
     port: u16,
+    #[serde(with = "humantime_serde")]
+    shutdown_timeout: Option<Duration>,
 }
 
 pub async fn serve(config: Config) -> Result<()> {
-    let Config { addr, port } = config;
+    let Config {
+        addr,
+        port,
+        shutdown_timeout,
+    } = config;
 
     let app = Router::new()
         .route("/", get(ready))
@@ -26,9 +36,22 @@ pub async fn serve(config: Config) -> Result<()> {
     let listener = TcpListener::bind((addr, port))
         .await
         .context("bind TcpListener")?;
-    axum::serve(listener, app).await.context("run server")
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal(shutdown_timeout))
+        .await
+        .context("run server")
 }
 
 async fn ready() -> impl IntoResponse {
     StatusCode::OK
+}
+
+async fn shutdown_signal(shutdown_timeout: Option<Duration>) {
+    signal(SignalKind::terminate())
+        .expect("install SIGTERM handler")
+        .recv()
+        .await;
+    if let Some(shutdown_timeout) = shutdown_timeout {
+        sleep(shutdown_timeout).await;
+    }
 }
