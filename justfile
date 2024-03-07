5f96d0594a9d0494688dfa8f450bf4d39e664b17
Heiko Seeberger <hseeberger@users.noreply.github.com>
2024-03-07 07:44:30 +0100

    chore: cleanup (#41)
    * chore: cleanup
    
    * fix-justfile

--- justfile
+++ justfile
@@ -1,13 +1,13 @@
 set shell := ["bash", "-uc"]
 
 check:
-	cargo check --tests
+	cargo check
 
-fmt:
-	cargo +nightly fmt
+fmt toolchain="+nightly":
+	cargo {{toolchain}} fmt
 
-fmt-check:
-	cargo +nightly fmt --check
+fmt-check toolchain="+nightly":
+	cargo {{toolchain}} fmt --check
 
 lint:
 	cargo clippy --no-deps -- -D warnings
@@ -15,16 +15,13 @@ lint:
 test:
 	cargo test
 
-coverage:
-	cargo llvm-cov --all-features --workspace --lcov --output-path lcov.info
-
 fix:
 	cargo fix --allow-dirty --allow-staged
 
 all: check fmt lint test
 
 run port="8080":
-	RUST_LOG=hello_rs=debug,api_version=debug,info \
+	RUST_LOG=hello_rs=debug,api_version=debug,tower_http=debug,info \
 		APP__API__PORT={{port}} \
 		APP__PG_SERVICE_REPOSITORY__PASSWORD=hello-rs \
 		cargo run -p hello-rs
--- src/api/mod.rs
+++ src/api/mod.rs
@@ -2,7 +2,7 @@ mod v0;
 
 use anyhow::{Context, Result};
 use api_version::api_version;
-use axum::{Router, ServiceExt};
+use axum::{body::Body, http::Request, Router, ServiceExt};
 use serde::Deserialize;
 use std::{net::IpAddr, time::Duration};
 use tokio::{
@@ -12,6 +12,7 @@ use tokio::{
 };
 use tower::{Layer, ServiceBuilder};
 use tower_http::trace::TraceLayer;
+use tracing::{field, info_span, Span};
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "kebab-case")]
@@ -31,7 +32,7 @@ pub async fn serve(config: Config) -> Result<()> {
 
     let app = Router::new()
         .nest("/v0", v0::app())
-        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
+        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http().make_span_with(make_span)));
 
     let app = api_version!(0..=0).layer(app);
 
@@ -52,4 +53,10 @@ async fn shutdown_signal(shutdown_timeout: Option<Duration>) {
     if let Some(shutdown_timeout) = shutdown_timeout {
         sleep(shutdown_timeout).await;
     }
+}
+
+fn make_span(request: &Request<Body>) -> Span {
+    let headers = request.headers();
+    let path = request.uri().path();
+    info_span!("incoming request", path, ?headers, trace_id = field::Empty)
 }
